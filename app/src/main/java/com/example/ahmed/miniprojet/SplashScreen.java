f978b03d7c2a30251a475ff53a27cddb2c461f48
tarektaamali <tarek.taamali@gmail.com>
2016-12-05 13:59:32 +0100

    splashscreen 1

---
+++ app/src/main/java/com/example/ahmed/miniprojet/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.ahmed.miniprojet;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen  extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
