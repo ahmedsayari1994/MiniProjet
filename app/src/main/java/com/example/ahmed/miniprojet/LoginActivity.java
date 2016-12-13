335af5478800421e984479b824fef7e396ebb667
tarektaamali <tarek.taamali@gmail.com>
2016-12-13 14:00:56 +0100

    login with facebookk version 0.1

--- app/src/main/java/com/example/ahmed/miniprojet/LoginActivity.java
+++ app/src/main/java/com/example/ahmed/miniprojet/LoginActivity.java
@@ -2,15 +2,33 @@ package com.example.ahmed.miniprojet;
 
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -19,19 +37,73 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView tx;
+    TextView tx; String text,text1="";
     EditText ed1,ed2;
     Button btn;
+    LoginButton login;
+    private String facebook_id,f_name, m_name, l_name, gender, profile_image, full_name, email_id;
+
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+
         tx=(TextView) findViewById(R.id.tvregistre);
         ed1=( EditText) findViewById(R.id.edlogin1);
         ed2=(EditText) findViewById(R.id.edpassword1);
+        login = (LoginButton)findViewById(R.id.login_button);
+        login.setReadPermissions("public_profile email");
+        getKeyHash();
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
 
+                facebook_id=f_name= m_name= l_name= gender= profile_image= full_name= email_id="";
+
+                if(AccessToken.getCurrentAccessToken() != null){
+
+                    Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                    RequestData();
+                    Toast.makeText(getApplicationContext(),f_name,Toast.LENGTH_SHORT).show();
+                    System.out.println(f_name);
+                    startActivity(i);
+                    Profile profile = Profile.getCurrentProfile();
+                    if (profile != null) {
+                        facebook_id=profile.getId();
+                        Log.e("facebook_id", facebook_id);
+                        f_name=profile.getFirstName();
+                        Log.e("f_name", f_name);
+                        m_name=profile.getMiddleName();
+                        Log.e("m_name", m_name);
+                        l_name=profile.getLastName();
+                        Log.e("l_name", l_name);
+                        full_name=profile.getName();
+                        Log.e("full_name", full_name);
+                        profile_image=profile.getProfilePictureUri(400, 400).toString();
+                        Log.e("profile_image", profile_image);
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+            }
+        });
 
         tx.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,5 +199,60 @@ public class LoginActivity extends AppCompatActivity {
         }catch (Exception ex){}
 
         return linereultcal;
+    }
+    private void getKeyHash() {
+
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo("navigationloginsample.android.com.loginsample", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
+    }
+    public void RequestData(){
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object,GraphResponse response) {
+
+                JSONObject json = response.getJSONObject();
+
+                try {
+                    if(json != null){
+                         text = "<b>Name :</b> "+json.getString("name")+"<br><br><b>Email :</b> "+json.getString("email")+"<br><br><b>Profile link :</b> "+json.getString("link");
+                     text1=json.getString("name");
+                        System.out.println("name :"+text1);
+
+                        f_name=text1;
+                        // details_txt.setText(Html.fromHtml(text));
+                      //  profile.setProfileId(json.getString("id"  System.out.println("Json data :"+json);));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link,email,picture");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
